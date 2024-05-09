e0c445f6f68502deba35a9f7127b8865243ce266
priyanka-neggi <117040051+priyanka-neggi@users.noreply.github.com>
2024-05-09 18:30:05 +0530

    tracker

---
+++ public/js/tracker.js
@@ -0,0 +1,75 @@
+// SIDEBAR TOGGLE
+
+let sidebarOpen = false;
+const sidebar = document.getElementById('sidebar');
+
+function openSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add('sidebar-responsive');
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove('sidebar-responsive');
+    sidebarOpen = false;
+  }
+}
+
+// ---------- CHARTS ----------
+
+// AREA CHART
+const areaChartOptions = {
+  series: [
+    {
+      name: 'money saved',
+      data: [31, 40, 28, 51, 42, 109, 100],
+    },
+    {
+      name: 'Carbon',
+    //   data: [11, 32, 45, 32, 34, 52, 41],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: 'area',
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ['#114232', '#246dec'],
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: 'smooth',
+  },
+  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+  markers: {
+    size: 0,
+  },
+  yaxis: [
+    {
+      title: {
+        text: 'money Saved',
+      },
+    },
+    // {
+    //   opposite: true,
+    //   title: {
+    //     text: 'carbon',
+    //   },
+    // },
+  ],
+  tooltip: {
+    shared: true,
+    intersect: false,
+  },
+};
+
+const areaChart = new ApexCharts(
+  document.querySelector('#area-chart'),
+  areaChartOptions
+);
+areaChart.render();
